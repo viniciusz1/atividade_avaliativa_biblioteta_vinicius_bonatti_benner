61f80187280e668d8e70053d9ad63a92cd40d5c6
viniciusz1 <vinicius101313@gmail.com>
2022-06-27 20:58:20 -0300

    po

--- router.js
+++ router.js
@@ -2,8 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 const autores = require('./api/autores/autores.controller');
+const clientes = require('./api/clientes/clientes.controller');
+const editoras = require('./api/editoras/editoras.controller');
+const livros = require('./api/livros/livros.controller');
+const locacoes = require('./api/locacoes/locacoes.controller');
 
 
 router.use('/autores', autores);
+router.use('/clientes', clientes);
+router.use('/editoras', editoras);
+router.use('/livros', livros);
+router.use('/locacoes', locacoes);
 
 module.exports = router;
--- sandbox.js
+++ sandbox.js
@@ -14,11 +14,16 @@ async function deletar(){
     const dados = await crud.remove('pessoas', 'FbI8reVMAb90RveeTfVb')
     console.log(dados);
 }
-async function deletar(){
-    const dados = await crud.dados('pessoas', 'FbI8reVMAb90RveeTfVb')
+async function adicionar(){
+    const objeto = {
+        sim: "aghan",
+        nao: "osifdjao"
+    }
+    const dados = await crud.save('pessoas', 'FbI8reVMAb90RveeTfVb', objeto)
     console.log(dados);
 }
 
-buscarDados()
+adicionar();
+// buscarDados()
 // buscarDadoid()
 // deletar();
