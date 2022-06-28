0cb3a9157d430d253ee50fe685b9f655edbd3a66
viniciusz1 <vinicius101313@gmail.com>
2022-06-27 21:17:17 -0300

    p

--- api/autores/autores.controller.js
+++ api/autores/autores.controller.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const autoresHandleer = require('./autores.handler')
 
 router.get('/', (req, res) => {
-    
+    res.send("<h1>O BONATTI É MUITO LINDO</h1>")
 })
 
 module.exports = router;
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.use(express.json());
 
 app.use('/api', router);
 
+
 app.listen(8080, () => {
     console.log('App listen on http://localhost:8080');
 })
