4e70845d32f93ff0b7d01b51e25eed8a39f9a97b
viniciusz1 <vinicius101313@gmail.com>
2022-07-27 11:41:48 -0300

    locacao com mesmo id ok

--- api/locacoes/locacoes.handler.js
+++ api/locacoes/locacoes.handler.js
@@ -14,19 +14,30 @@ async function verificaSeClienteJaTemUmAluguel(idCliente){
 async function adicionarLocacao(dadosParametro){// retorno idCliente: 2, livros: [ 1, 2, 3, 4, 5 ]
     const locacaoCliente = await verificaSeClienteJaTemUmAluguel(dadosParametro.idCliente)
     if(!locacaoCliente){ // caso o cliente já ter um aluguel -- ok
+        
+        
+        const dados = await crud.save('locacoes', null, {idCliente: dadosParametro.idCliente})
+        let destruirLocacao = true
+        
         for(let i of dadosParametro.livros){ //Atribui os valores a tabela locacoesELivros
             const estaAlugado = await verificaSeLivroJaFoiAlugado(i)
             if(estaAlugado){
                 console.log("acho que ta alugado mano")
             }else{
-                const dados = await crud.save('locacoes', null, {idCliente: dadosParametro.idCliente})
                 await crud.save('locacoesELivros', null, {
                     idLocacoes: dados.id,
                     isbnLivro: i
                 })
+                destruirLocacao = false
                 console.log("acho que não ta alugado mano")
             }         
         }
+
+        if(destruirLocacao){
+            const teste = await crud.remove('locacoes', dados.id.toString())
+            console.log(teste)
+        }
+
     }else{
         console.log("este cliente já tem um livro em locação")
     }
